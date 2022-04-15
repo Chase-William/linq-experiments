f423fb0d54f4f2c392c214b339daba8b4ed8a5fe
cclcr21 <cclcr21@rit.edu>
2022-04-15 10:46:06 -0400

    initial commit

---
+++ DynamicLinq/Data.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicLinq
+{
+    public static class Data
+    {
+        public static List<Customer> Get() => new List<Customer>()
+		{
+			new Customer() { CustomerID = 1, Name = "Terri	Lee	Duffy"},
+			new Customer() { CustomerID = 2, Name = "Gail A Erickson"},
+			new Customer() { CustomerID = 3, Name = "Gigi N Matthew"},
+			new Customer() { CustomerID = 4, Name = "Michael Raheem"},
+			new Customer() { CustomerID = 5, Name = "Janice Galvin"},
+			new Customer() { CustomerID = 6, Name = "Michael Sullivan"},
+			new Customer() { CustomerID = 7, Name = "David Bradley"},
+			new Customer() { CustomerID = 8, Name = "David	Benshoof"},
+			new Customer() { CustomerID = 9, Name = "Peter	Gilbert"},
+			new Customer() { CustomerID = 10, Name = "Rebecca Bradley"},
+		};
+	}
+
+	public class Customer
+	{
+		public int CustomerID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public DateTime LastLogin { get; set; }
+		public Boolean IsActive { get; set; }
+	}
+}
---
+++ DynamicLinq/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
+using System.Reflection;
+
+namespace DynamicLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Normal static collection
+            Data.Get()
+                .Where(x => x.IsActive == true)
+                .OrderBy(x => x.Name)
+                .Select(x => new { x.Name }); // using infered prop Name from x for new type
+
+            // Kinda of useless dynamic linq query as a though experiment
+            {
+                var whereBy = WhereByOptions.NameContains; // option for first query
+                var orderBy = OrderByOptions.LoginDateDescending;
+                var propOfInterest = PropertyOfInterest.LoginDateAndName;
+
+                var rawList = Data.Get();
+
+                // Stage #1 -- Where Query
+                IEnumerable<Customer> queriedData = whereBy == WhereByOptions.NameContains ? rawList.Where(x => x.Name.Contains("G")) : rawList.Where(x => x.IsActive == true);
+
+                // Stage #2 -- OrderBy Query
+                queriedData = new Lazy<IEnumerable<Customer>>(() => // Essentially an IIFE
+                {
+                    return orderBy switch // C# 8.0 switch https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-8
+                    {
+                        OrderByOptions.NameAscending => queriedData.OrderBy(x => x.Name),
+                        OrderByOptions.NameDescending => queriedData.OrderByDescending(x => x.Name),
+                        OrderByOptions.LoginDateAscending => queriedData.OrderBy(x => x.LastLogin),
+                        OrderByOptions.LoginDateDescending => queriedData.OrderByDescending(x => x.LastLogin),
+                        _ => throw new Exception(""),
+                    };
+                }).Value;
+
+                // Stage #3 -- Select Props
+                var result = new Lazy<IEnumerable<object>>(() =>
+                {
+                    return propOfInterest switch
+                    {
+                        PropertyOfInterest.Name => queriedData.Select(x => new { x.Name }),
+                        PropertyOfInterest.LoginDateAndName => queriedData.Select(x => new { x.LastLogin, x.Name }),
+                        PropertyOfInterest.Description => queriedData.Select(x => new { x.Name }),
+                        _ => throw new Exception("")
+                    };
+                }).Value;
+
+                // Get an element from results
+                var obj = result.FirstOrDefault();
+                if (obj == null) // Check collection not empty
+                {
+                    Console.WriteLine("Query returned empty collection...");
+                    return;
+                }
+
+                // Print out each property using reflection to get them dynamically like a list (reminds me of JS)
+                foreach (var prop in obj.GetType().GetRuntimeProperties()) {
+                    Console.WriteLine($"Has Name: {prop.Name} with value of: {prop.GetValue(obj)}");
+                }
+            }
+        }
+
+
+    }
+
+    enum WhereByOptions
+    {
+        NameContains,
+        IsActive
+    }
+
+    enum OrderByOptions
+    {
+        NameAscending,
+        NameDescending,
+        LoginDateAscending,
+        LoginDateDescending
+    }
+
+    enum PropertyOfInterest
+    {
+        Name,
+        LoginDateAndName,
+        Description
+    }
+}
